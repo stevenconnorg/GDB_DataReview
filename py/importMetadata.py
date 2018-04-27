5774048a2bd6472b0dec63afee721a69012d8e82
stevenconnorg <stevenconnorg@gmail.com>
2018-04-27 13:52:34 -0500

    fix path to feature class and glob for source xml file

--- py/importMetadata.py
+++ py/importMetadata.py
@@ -4,24 +4,34 @@ Created on Fri Apr 27 12:46:58 2018
 
 @author: stevenconnorg
 """
-import arcpy 
+import arcpy , glob, os
 
 gdb = arcpy.GetParameterAsText(0)
 inMetadataDir = arcpy.GetParameterAsText(1)
 importType = arcpy.GetParameterAsText(2) # "FROM_FGDC"
 autoUpdate = arcpy.GetParameterAsText(3) # "ENABLED"
 
-FDSs = arcpy.ListDatasets(gdb)
+
+gdb = r"C:\Users\stevenconnorg\Documents\knight-federal-solutions\CIP_DataReview\archive\ANG_Peoria  - Copy\Non_Network_CIP\ANG_Peoria_CIP.gdb"
+inMetadataDir = r"C:\Users\stevenconnorg\Documents\knight-federal-solutions\CIP_DataReview\archive\ANG_Peoria  - Copy\Non_Network_CIP\METADATA"
+importType = "FROM_FGDC"
+autoUpdate = "ENABLED"
+
+arcpy.env.workspace = gdb
+FDSs = arcpy.ListDatasets()
 
 if not FDSs:
-    FCs = arcpy.ListFeatureClasses(gdb)
+    FCs = arcpy.ListFeatureClasses(feature_dataset=gdb)
     for fc in FCs:
-        arcpy.ImportMetadata_conversion(Source_Metadata = inMetadataDir+"/"+fc+".xml", Import_Type=importType, Target_Metadata = fc, Enable_automatic_updates=autoUpdate)
-
+        inFile = glob.glob(inMetadataDir+"/"+fc+"*")[0]
+        arcpy.ImportMetadata_conversion(Source_Metadata = inFile, Import_Type=importType, Target_Metadata = os.path.join(gdb,fc), Enable_automatic_updates=autoUpdate)
 
 for fds in FDSs:
-    arcpy.ImportMetadata_conversion(inMetadataDir+"/"+fds+".xml", Import_Type=importType, Target_Metadata = fc, Enable_automatic_updates=autoUpdate)
+    inFile = glob.glob(inMetadataDir+"/"+fds+".xml")[0]
+    arcpy.ImportMetadata_conversion(inFile, Import_Type=importType, Target_Metadata = fc, Enable_automatic_updates=autoUpdate)
     FCs = arcpy.ListFeatureClasses(feature_dataset=fds)
     for fc in FCs:        
-        arcpy.ImportMetadata_conversion(inMetadataDir+"/"+fds+"_"+fc+".xml", Import_Type=importType, Target_Metadata = fc, Enable_automatic_updates=autoUpdate)
+        inFile = glob.glob(inMetadataDir+"/"+"*"+fc+"*")[0]
+        arcpy.ImportMetadata_conversion(Source_Metadata = inFile,  Target_Metadata = os.path.join(gdb,fds,fc), Enable_automatic_updates=autoUpdate)
 
+# Import_Type=importType,
