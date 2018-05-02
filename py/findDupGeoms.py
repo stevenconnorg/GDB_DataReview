317899c973874c1552ab9325a3fa8a8aeed0c558
stevenconnorg <stevenconnorg@gmail.com>
2018-05-02 15:00:58 -0500

    add some more informative messages

--- py/findDupGeoms.py
+++ py/findDupGeoms.py
@@ -33,49 +33,53 @@ keysLen = len(keys)
 outTbl[0] = keys
 for dataset in arcpy.ListDatasets():  
     for fc in arcpy.ListFeatureClasses('','',dataset):
-
-        # Find duplicate geometry
-        dupeTable = "in_memory\\tmp"
-        arcpy.FindIdentical_management(fc, "in_memory\\tmp", ["Shape"],xy_tolerance= xyTolerance, z_tolerance = zTolerance, output_record_option="ONLY_DUPLICATES")
-        
-        # Get table count and pass the dataset if no duplicates exist
-        tblCount = arcpy.GetCount_management(dupeTable)
-        print fc + " duplicate feature count: " + str(tblCount)
-        arcpy.AddMessage(fc + " duplicate feature count: " + str(tblCount))
-        if int(tblCount[0]) == 0:
-            arcpy.Delete_management("in_memory\\tmp") 
-            continue
-        
-        
-        
+        fcCount = arcpy.GetCount_management(fc)
+        if int(fcCount[0]) == 0:
+            arcpy.AddMessage("No features in "+fc+" ... skipping!")
+            pass
         else:
-			# Get Object IDs of duplicate values to build expression for layer file
-            fidList=[]
-            with arcpy.da.SearchCursor(dupeTable,["IN_FID"]) as cursor:
-                for row in cursor:
-                    fidList.append(row[0])
-            expression = 'OBJECTID IN ({0})'.format(', '.join(map(str, fidList)) or 'NULL')
-
-			# Save Find Identical table and delete in memory table
-            outLyrName = "_dupeGeom_"+fc
-            uniqdupCount = len(unique_values(dupeTable , "FEAT_SEQ"))
-            dupCount = len(unique_values(dupeTable , "IN_FID"))
+            # Find duplicate geometry
+            dupeTable = "in_memory\\tmp"
+            arcpy.FindIdentical_management(fc, "in_memory\\tmp", ["Shape"],xy_tolerance= xyTolerance, z_tolerance = zTolerance, output_record_option="ONLY_DUPLICATES")
             
-            fidList.sort()
-            FIDs =  ', '.join(map(str, fidList))
-            n= n+1
-            values= [n,dataset,fc,FIDs,str(dupCount) +" duplicates across "+str(uniqdupCount)+" features. "]
-
-            outTbl.append(values)
-              
-            #arcpy.TableToTable_conversion(dupeTable, outputDir, outLyrName)
-            arcpy.MakeFeatureLayer_management(fc, outLyrName, expression)
+            # Get table count and pass the dataset if no duplicates exist
+            tblCount = arcpy.GetCount_management(dupeTable)
+            print fc + " duplicate feature count: " + str(tblCount)
+            arcpy.AddMessage(fc + " duplicate feature count: " + str(tblCount))
+            if int(tblCount[0]) == 0:
+                arcpy.Delete_management("in_memory\\tmp") 
+                continue
             
             
-            arcpy.SaveToLayerFile_management(outLyrName, os.path.join(outputLayers,outLyrName),"ABSOLUTE","10.3")
-            arcpy.Delete_management(dupeTable)
-            del outLyrName
-            del dupeTable
+            
+            else:
+    			# Get Object IDs of duplicate values to build expression for layer file
+                fidList=[]
+                with arcpy.da.SearchCursor(dupeTable,["IN_FID"]) as cursor:
+                    for row in cursor:
+                        fidList.append(row[0])
+                expression = 'OBJECTID IN ({0})'.format(', '.join(map(str, fidList)) or 'NULL')
+    
+    			# Save Find Identical table and delete in memory table
+                outLyrName = "_dupeGeom_"+fc
+                uniqdupCount = len(unique_values(dupeTable , "FEAT_SEQ"))
+                dupCount = len(unique_values(dupeTable , "IN_FID"))
+                
+                fidList.sort()
+                FIDs =  ', '.join(map(str, fidList))
+                n= n+1
+                values= [n,dataset,fc,FIDs,str(dupCount) +" duplicates across "+str(uniqdupCount)+" features. "]
+    
+                outTbl.append(values)
+                  
+                #arcpy.TableToTable_conversion(dupeTable, outputDir, outLyrName)
+                arcpy.MakeFeatureLayer_management(fc, outLyrName, expression)
+                
+                
+                arcpy.SaveToLayerFile_management(outLyrName, os.path.join(outputLayers,outLyrName),"ABSOLUTE","10.3")
+                arcpy.Delete_management(dupeTable)
+                del outLyrName
+                del dupeTable
 
 import csv
 
